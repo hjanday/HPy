be0677507974756f07cbdd686c999a1feff85ef2
Harpreet Janday <54457483+janday-harpreet@users.noreply.github.com>
2021-02-11 21:35:28 -0500

    Update botcode.py

--- HPy-Bot/botcode.py
+++ HPy-Bot/botcode.py
@@ -5,15 +5,16 @@ import requests
 from discord.ext import commands
 from datetime import date,datetime
 import calendar
-import sys
+import random
 import os
+import sys
+t = open("TOKEN.txt", "r").read()
 
 client = discord.Client()
 client = commands.Bot(command_prefix= 'h!')
 
 magicwords = ["It is certain.", "For sure.", "Without a doubt.", "Most likely.", "Signs point to yes.", "Ask again later.", "Better not tell you now.", " Cannot predict now.",  "Nope, ain't gonna happen", "Haha, that's funny", "No way", "Maybe"]
 
-t = open("TOKEN.txt", "r").read()
 
 @client.event
 async def on_ready():
@@ -28,7 +29,7 @@ async def member_join(member):
 async def member_leave(member):
     print(f'{member} has left the server')
 
-    
+
 # client command lines
 @client.command()
 # async def [command name] - type in prefix then command 
@@ -36,15 +37,7 @@ async def magic8ball(context, userarg):
     await context.send(rnd.choice(magicwords))
 
 
-@client.command()
-async def isFriday(context):
-    today = date.today()
-    if(calendar.day_name[today.weekday()] == 'Friday'):
-        await context.send("ITS FRIDAYY")
-        await context.send("https://tenor.com/view/yakuza-kazuma-kiryu-goro-majima-disco-friday-gif-16566350")
-    else:
-        await context.send("It's not Friday :c")
-        await context.send("https://tenor.com/view/pepe-why-pepe-the-frog-sad-crying-gif-16026853")
+
 @client.command()
 async def currTime(context):
     now = datetime.now()
@@ -54,6 +47,20 @@ async def currTime(context):
 
     await context.send(currDate)
     await context.send(time_and_day)
+
+
+@client.command()
+async def diceroll(context, user:discord.Member):
+    diceval_user = random.randrange(1,6,1)
+    diceval_opponent = random.randrange(1,6,1)
+    if(int(diceval_user)> int(diceval_opponent)):
+        await context.send(f'You rolled {diceval_user} and opponent rolled {diceval_opponent}, they win')
+    elif(int(diceval_user) == int(diceval_opponent)):
+        await context.send(f'You rolled {diceval_user} and opponent rolled {diceval_opponent}, you have tied')
+    else:
+         await context.send(f'You rolled {diceval_user} and opponent rolled {diceval_opponent}, you win')
+
+
 
 
 #Turn to embed menu
@@ -66,40 +73,27 @@ async def helpme(context):
     embed.add_field(name = "h!bingme", value="Returns top 3 searches from bing [In Progress]")
     embed.add_field(name = "h!delmsg", value = "Deletes a set number of messages [Admin Only]") 
     embed.add_field(name = "h!getboing", value = "Pings User [Admin Only]") 
-    embed.add_field(name = "h!invserver", value = "Creates a permanent server invite")
     embed.add_field(name = "h!currTime", value = "gives current time") 
     embed.add_field(name = "h!isFriday", value = "is it friday") 
     embed.add_field(name = "h!ipermInv", value = "gets a perm invite to server") 
-    embed.add_field(name = "h!ezmath", value = "evaluate an expression") 
+    embed.add_field(name = "h!motivate", value = "motivates you") 
+    embed.add_field(name = "h!rhyme", value = "gets rhyme") 
+    
+    #minigames
+    
+    #minigames
+    embed.add_field(name = "h!diceroll", value = "rolls dice") 
+    
     await context.send(content = None, embed = embed)
 
 
 
 
 
-@client.command()
-async def ezmath(context,eqn):
-    await context.send(eval(str(eqn)))
 
 
-@client.command()
-async def permInv(context):
-    await context.send("your invite code")
-
-@client.command()
-async def gm (context):
-    await context.send("Good morning quarantine kids, stay inside and stay alive")
-
-
-@client.command()
-async def gn(context):
-    await context.send("Good night people, go sleep now")
-
-
-
-@client.command()
-@commands.has_any_role('insert role')
-async def bingme(context, usersearch):
+@client.command() 
+async def bingme(context, *, usersearch):
     userarg = {"q":usersearch}
     link = requests.get("https://bing.com/search", params=userarg)
 
@@ -118,6 +112,7 @@ async def bingme(context, usersearch):
         obj_href = obj.find("a").attrs["href"]
         await context.send(objtxt)
         await context.send(obj_href)
+        
 
 
 @client.command()
@@ -128,30 +123,48 @@ async def motivate(context, user:discord.Member):
 
 
 @client.command()
-@commands.has_any_role('Tech Support')
+@commands.has_any_role('Bot Dev')
 async def shutdown(context):
     await context.send("See ya, HPy gonna sleep now")
     await client.change_presence(activity=discord.Game(name='Currently Offline'), status=discord.Status.do_not_disturb)
 
 
+#Moderation Commands
+@client.command()
+@commands.has_any_role('Admin')
+async def delmsg (context, userarg):
+    await context.channel.purge(limit = int(userarg))
+
+@client.command()
+async def rhyme(context, word, num):
+    chosen_word = {"rel_rhy":word}
+
+    request= requests.get('https://api.datamuse.com/words', chosen_word)
+
+    json_rhyming = request.json()
+
+
+    for each_rhyme in json_rhyming[0:int(num)]:
+        await context.send(each_rhyme['word'])
 
 
 @client.command()
-@commands.has_any_role('insert role')
-async def getboing(context, user:discord.Member, userarg):
-    if int(userarg) > 999:
-        await context.send("Stop pinging too much")
-    else:
-        for i in range(int(userarg)):
-            await context.send(f'Pinged,  {user.mention}')
+@commands.has_any_role('Admin')
+async def kickuser(context, user: discord.Member, *, userarg):
+    await user.kick(reason = userarg)
+    await context.send(f'Kicked {user.mention}')
 
 
-for each_cog in os.listdir('.//cogs'):
-    if each_cog.endswith(".py"):
-        try:
-            each_cog = f"cogs.{each_cog.replace('.py', '')}"
-            client.load_extension(each_cog)
-        except Exception as ec:
-            print(f"{each_cog} cannot be added, please fix")
-            raise ec
+@client.command()
+@commands.has_any_role('Admin')
+async def banuser(context, user: discord.Member, *, userarg):
+    await user.ban(reason = userarg)
+    await context.send(f'Banned {user.mention}')
+
+
+
+
+
+
+
 client.run(t)
